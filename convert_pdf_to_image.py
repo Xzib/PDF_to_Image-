b7b51ee67f0ae4e3b9545db7dd4292d09899a8e1
Xzib <syedzohaibali94@gmail.com>
2019-12-20 22:26:19 +0500

    updated the convert_pdf_to_image and updated environment.yml

--- convert_pdf_to_image.py
+++ convert_pdf_to_image.py
@@ -1,11 +1,13 @@
-def pdf_to_imge(path):
+def pdf_to_imge(file_name):
     from PIL import Image
     from pdf2image import convert_from_path
     import os
-    path = path + ".pdf"
+    file_name = file_name + ".pdf"
         
     try:
-        images_from_path = convert_from_path(os.path.abspath(path))
+        print(os.path.relpath(file_name))
+        images_from_path = convert_from_path(os.path.relpath(file_name))
+        print(images_from_path)
         counter = 1
         os.chdir(r".\pdf_to_jpg")
         for image in images_from_path:
@@ -22,7 +24,7 @@ def pdf_to_imge(path):
     
 
 if __name__ == "__main__":
-    path = r"D:\Zohaib\Fivrr\pdf_to_image\PDF_to_Image-\file-example_PDF_1MB.pdf"
-    pdf_to_imge(path)
+    file_name = "lease_example"
+    pdf_to_imge(file_name)
 
     
