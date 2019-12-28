a00836856df4a0283b2c1a06df30de7f3f48b436
Xzib <syedzohaibali94@gmail.com>
2019-12-28 17:57:51 +0500

    new change regarding the system, now I have made a directory for the pdf_files, and guide the convert_pdf file to that dir from here I have to convert files into images and then OCR all the pages to look for all possible instances of the word

--- convert_pdf_to_image.py
+++ convert_pdf_to_image.py
@@ -1,37 +1,34 @@
-def pdf_to_imge(file_name):
+def pdf_to_imge(path):
     from PIL import Image
     from pdf2image import convert_from_path
     import os
-    file_name = file_name + ".pdf"
-        
-    try:
-<<<<<<< HEAD
-        images_from_path = convert_from_path(os.path.abspath(path),dpi=500)
-=======
-        print(os.path.relpath(file_name))
-        images_from_path = convert_from_path(os.path.relpath(file_name))
-        print(images_from_path)
->>>>>>> 2cfcfc3c529e27e12294e3ff2e5695cd8ffddc97
-        counter = 1
-        os.chdir(r".\pdf_to_jpg")
-        for image in images_from_path:
-            image.save(str(counter)+'_page'+".jpg")
-            counter += 1
-    except:
-        return "OOPS! Something went wrong.. Are you sure the file is present in the directory"
-    else:
-        return "Operation Finished successfully"
+    os.chdir(os.path.relpath(".\\pdf_files"))
+    # x = os.getcwd()
+    # print(x)
+    # print(os.chdir(os.path.relpath(".\\pdf_files")))
+    for path, directories, files in os.walk("."):
+        for file in files:
+            print(file)
+            # file_name = file_name + ".pdf"
+                
+            # try:
+            #     images_from_path = convert_from_path(os.path.abspath(file),dpi=500)
+            #     counter = 1
+            #     os.chdir(r".\pdf_to_jpg")
+            #     for image in images_from_path:
+            #         image.save(str(counter)+'_page'+".jpg")
+            #         counter += 1
+            # except:
+            #     return "OOPS! Something went wrong.. Are you sure the file is present in the directory"
+            # else:
+            #     return "Operation Finished successfully"
 
 
     
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    path = r"yale_vna"
+    
+    path = r"."
     pdf_to_imge(path)
-=======
-    file_name = "lease_example"
-    pdf_to_imge(file_name)
->>>>>>> 2cfcfc3c529e27e12294e3ff2e5695cd8ffddc97
 
     
